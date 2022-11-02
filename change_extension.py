447057110289ded04273e2eb75e152285aa15a53
TGitGit <66289283+TGitGit@users.noreply.github.com>
2022-11-02 11:24:00 +0900

    Add files via upload
    複数ブックの拡張子を一括変換する

--- change_extension.py
+++ change_extension.py
@@ -1,68 +1,40 @@
-import pyautogui as pg
+"""
+一つのフォルダに入っている複数Excelブックの拡張子を一括変換する。なお、xlsmへの変換は現時点では出来ない。
+〇xlsm→xlsx
+〇xls→xlsx
+〇xlsx→xls
+〇xlsm→xls
+×xls→xlsm
+×xlsx→xlsm
+files,changed,extensionの3個の変数を事前設定して実行する
+"""
+
+import os
+import win32com.client as win32
 import glob
-import subprocess
 
-from time import sleep
-from pyscreeze import ImageNotFoundException
+#変換したいExcelブックが入っているフォルダを指定
+files = glob.glob("D:/python/input_value_excel/様式0未挿入_オルソ修正箇所/土石流/**.xls")
+#変換したExcelを収めるフォルダを指定
+changed= os.path.abspath("D:/python/input_value_excel/様式0未挿入_オルソ修正箇所/土石流/xlsx")
+#拡張子を指定
+extension=".xlsx"
 
-pg.FAILSAFE = True  # 止めたいときは画面左上隅にマウスを持っていく
-psize = pg.size()
-print(psize)  # 画面サイズを表示
-#C:/Users/SEKIYA_T/AppData/Roaming/JetBrains/PyCharmCE2020.1/scratches/RPA
-excel = glob.glob(
-    "change_extension/**.xls*"
-)  # ここに入っているフォルダをリスト化してIfdirに格納
+excel = win32.gencache.EnsureDispatch("Excel.Application")
+for file in files:
+    f=os.path.splitext(os.path.basename(file))
 
-counter = 0  # 最初に1回だけ出力している場合は1から指定してください。0が最初となる
-for file in excel[counter:]:
-    # while counter<=len(Ifdir):
-    print(counter + 1, "/", len(excel))  # 進捗状況カウント
-    proc = subprocess.Popen(
-        [r"C:\Program Files (x86)\Microsoft Office\Office16\EXCEL.EXE", file],
-        shell=False,
+    wb = excel.Workbooks.Open(os.path.abspath(file))
+    excel.DisplayAlerts = False
+    wb.DoNotPromptForConvert = True
+    wb.CheckCompatibility = False
+
+    wb.SaveAs(
+        changed
+        + "/"
+        + os.path.splitext(os.path.basename(file))[0]
+        + extension,
+        FileFormat=51,
+        ConflictResolution=2,
     )
-
-    pg.hotkey("fn","f6")
-
-
-
-
-
-    ##pg.moveTo(560, 508)
-    pg.PAUSE = 0.2
-    # ss=pg.locateCenterOnScreen("C:/Users/SEKIYA_T/Pictures/systemSetting.png",confidence=0.7)
-    # pg.click(x=ss[0],y=ss[1])#システム設定
-    pg.click(20, 1)
-    pg.hotkey("alt", "enter")  #
-    # pg.keyUp('alt',"enter")
-    pg.keyDown("ctrl")
-    pg.press("tab")
-    pg.keyUp("ctrl")
-
-    pg.press("down",5)
-    # pg.press("down")
-    # pg.press("down")
-    # pg.press("down")
-    # pg.press("down")
-
-    pg.press("tab")
-    pg.press("enter")  # 追加
-    pg.hotkey("shift", "tab")
-    pg.hotkey("shift", "tab")
-
-    if counter == 0:
-        pg.press("down")
-        pg.press("up")
-    else:
-        pg.press("down", presses=counter)
-
-    pg.press("enter")
-    pg.press("enter")
-    pg.press("tab", 5)
-    pg.press("enter")
-    pg.press("down")
-
-    print(str(pdfdir[counter]) + u"をリンクがけしました")
-    print("------------------------------------------------------------------------")
-    counter = counter + 1
-    # if counter>len(Ifdir):break
+    excel.Application.Quit()
